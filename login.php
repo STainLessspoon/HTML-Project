dc03ce6433e6b855f389f24fb828d4e15d62d42d
root <root@localhost.localdomain>
2022-05-31 14:20:20 -0400

    PHP login files

--- login.php
+++ login.php
@@ -1,19 +1,46 @@
-<!doctype html>
+<?php
+session_start();
+
+if( isset($_SESSION['user'])!="" ) {
+	$username = trim($_POST['username']);
+	$pass = trim($_POST['pass']);
+	$password = hash('sha256', $pass);
+
+	$query = "select userid, username, pass from people where username=?";
+	$stmt = $pdo->prepare($query);
+	$stmt->execute([$username]);
+	$count = $stmt->rowCount();
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	
+	if( $count == 1 && $row['pass']==$password ) {
+		$_SESSION['user'] = $row['userid'];
+		header("Location: profile.php");
+	}
+	else {
+		$message = "Invalid Login";
+	}
+	$_SESSION['message'] = $message;
+}
+?>
+
+
 <html>
-<head>
-	<title>Samuel Latimer</title>
-</head>
+<head><title>Login_Samuel_Latimer</title></head>
 <body>
-<h1>Login</h1>
-<form><b>Username</b></form>
-<input type = "text"></input>
-<br>
-<form><b>Password</b></form>
-<input type = "text"></input>
-<br>
-<br>
-<input type = "submit"></submit>
+<p><h1>
+<?php
+	if (isset($message) ) {
+		echo $message;
+	}
+?>
+</h1></p>
+
+<form action="login.php" method="post">
+Username: <input type="text" name="username" /> <br /><br />
+Password: <input type="password" name="pass" /> <br /><br />
+<input type="submit" name="sca" value="Login" /><br /><br />
 
 
+</form> 
 </body>
 </html>
